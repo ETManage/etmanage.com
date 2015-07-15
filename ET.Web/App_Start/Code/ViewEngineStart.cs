c8e6c82a2b2591d4163faa59207e075f87d4ef6b
West <liuweisime@163.com>
2015-07-15 09:23:11 +0800

    MVC4TOMVC5

---
+++ ET.Web/App_Start/Code/ViewEngineStart.cs
@@ -0,0 +1,25 @@
+﻿using PrecompiledMvcViewEngineContrib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Web;
+
+namespace ET.Web
+{
+    public class ViewEngineStart
+    {
+        /// <summary>
+        /// 初始化注册预编译视图
+        /// </summary>
+        public static void Start()
+        {
+            Assembly asm = Assembly.GetExecutingAssembly();
+            var engine = new PrecompiledMvcEngine(new List<Assembly> {
+                Assembly.Load("ET.Weixin.Web")
+            });
+            System.Web.Mvc.ViewEngines.Engines.Insert(0, engine);
+            System.Web.WebPages.VirtualPathFactoryManager.RegisterVirtualPathFactory(engine);
+        }
+    }
+}
